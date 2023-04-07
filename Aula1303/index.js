b368232b591ac3c7046ca3ef3595f878ff867091
GustavSantos <gustavosantos4991@gmail.com>
2023-04-06 22:13:09 -0300

    Aula0604 Exercício e finalização do ETEC contas

--- Aula1303/index.js
+++ Aula1303/index.js
@@ -34,6 +34,7 @@ function operation(){
             deposit()
         }else if(action === 'Sacar'){
             console.log('Sacando')
+            withdraw()
         }else if(action === 'Sair'){
             console.log(chalk.bgBlue.black('Obrigado por utilizar o Contas ETEC.'))
             setTimeout(() => {
@@ -74,7 +75,7 @@ function buildAccount(){
 
         fs.writeFileSync(
             `accounts/${accountName}.json`,
-            '{"balance":0}',
+            '{"balance":0, "limit":1000}',
             function (err){
                 console.error(err)
             }
@@ -218,6 +219,28 @@ function removeAmount(accountName, amount){
     if(!amount){
         console.log(chalk.bgRed.black('O valor precisa ser informado!'))
         return withdraw()
+    }
+    
+    if(accountData.balance < amount){
+        console.log(chalk.bgYellow.black('Entrou no cheque especial!'))
+    }
+
+    if((accountData.balance + accountData.limit) < amount){
+        console.log(chalk.bgRed.black('Você não tem limite para saque!'))
+        return
+    }
+    else{
+        accountData.balance = parseFloat(accountData.balance) - parseFloat(amount)
+
+        fs.writeFileSync(
+            `accounts/${accountName}.json`,
+            JSON.stringify(accountData),
+            function (err){
+                console.log(err)
+            }
+        )
+    
+        console.log(chalk.green(`Foi sacado: ${amount} da conta ${accountName}.`))
     }
 }
 //#endregion
